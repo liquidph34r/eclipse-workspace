78779b3c9a1e38349cd1fadab4d12676bd41044c
Gjohnson8 <77544765+Gjohnson8@users.noreply.github.com>
2022-04-29 09:54:57 -0500

    GUI Update for StartUp

--- Oracle test/src/Startup.java
+++ Oracle test/src/Startup.java
@@ -31,6 +31,9 @@ public class Startup extends JFrame {
 	CusLogin c1 = new CusLogin();
 	public static String Email = "";
 	public static String Password = "";
+	private JTextField eUser;
+	private JTextField ePass;
+	private final JLabel lblpassword_2 = new JLabel("");
 
 	/**
 	 * Launch the application.
@@ -107,5 +110,66 @@ public class Startup extends JFrame {
 		lblpassword.setBounds(69, 147, 151, 14);
 		
 		contentPane.add(lblpassword);
+		
+		lblpassword_2.setForeground(Color.RED);
+		lblpassword_2.setBounds(106, 11, 74, 14);
+		
+		contentPane.add(lblpassword_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Email:");
+		lblNewLabel_3.setBounds(240, 81, 46, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Password");
+		lblNewLabel_1_1.setBounds(240, 119, 46, 14);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Employee Login");
+		lblNewLabel_2_1.setBounds(326, 11, 74, 14);
+		contentPane.add(lblNewLabel_2_1);
+		
+		eUser = new JTextField();
+		eUser.setColumns(10);
+		eUser.setBounds(291, 78, 153, 20);
+		contentPane.add(eUser);
+		
+		ePass = new JTextField();
+		ePass.setColumns(10);
+		ePass.setBounds(291, 116, 153, 20);
+		contentPane.add(ePass);
+		
+		JButton btnEmpLogin_1 = new JButton("Login!");
+		btnEmpLogin_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				boolean res = hello.user_login(eUser.getText(),ePass.getText());
+				if(res == false) {
+					lblpassword_2.setText("Incorrect Password");
+				} else {
+					Email = eUser.getText();
+					Password = ePass.getText();
+					new CusLogin().setVisible(true);
+					dispose();
+					}
+			}
+		});
+		btnEmpLogin_1.setBounds(326, 174, 89, 23);
+		contentPane.add(btnEmpLogin_1);
+		lblpassword_2.setForeground(Color.RED);
+		lblpassword_2.setBounds(291, 147, 151, 14);
+		
+		contentPane.add(lblpassword_2);
+		
+		JButton SignUp = new JButton("Sign Up");
+		SignUp.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				SignUp signUp1 = new SignUp();
+				signUp1.setVisible(true);
+				dispose();
+			}
+		});
+		SignUp.setBounds(197, 227, 89, 23);
+		contentPane.add(SignUp);
 	}
 }
