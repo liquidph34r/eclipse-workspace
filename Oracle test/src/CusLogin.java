16c10fb0686c9d2887bfbf394b704c5284761c4f
liquidph34r <57736737+liquidph34r@users.noreply.github.com>
2022-05-04 15:55:40 -0500

    Fixing merge errors

--- Oracle test/src/CusLogin.java
+++ Oracle test/src/CusLogin.java
@@ -37,7 +37,7 @@ public class CusLogin extends JFrame {
 	private final JLabel lblNewLabel_1_2_1 = new JLabel("Last Name:");
 	private final JTextField lblFName = new JTextField();
 	private final JTextField lblLName = new JTextField();
-	private final JTextField lblEmail = new JTextField();
+	private final JTextField Email = new JTextField();
 	private final JTextField lblPassword = new JTextField();
 	private final JLabel lblNewLabel_2 = new JLabel("Basic Information");
 	private final JLabel Country = new JLabel("Country:");
@@ -75,9 +75,9 @@ public class CusLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public CusLogin() {
-		lblUserID.setEditable(false);
-		lblUserID.setBounds(62, 221, 86, 20);
-		lblUserID.setColumns(10);
+		UserID.setEditable(false);
+		UserID.setBounds(62, 221, 86, 20);
+		UserID.setColumns(10);
 		initGUI();
 	}
 	private void initGUI() {
@@ -164,11 +164,11 @@ public class CusLogin extends JFrame {
 		lblLName.setBounds(86, 126, 142, 20);
 		
 		contentPane.add(lblLName);
-		lblEmail.setEditable(false);
-		lblEmail.setColumns(10);
-		lblEmail.setBounds(52, 153, 142, 20);
+		Email.setEditable(false);
+		Email.setColumns(10);
+		Email.setBounds(52, 153, 142, 20);
 		
-		contentPane.add(lblEmail);
+		contentPane.add(Email);
 		lblPassword.setEditable(false);
 		lblPassword.setColumns(10);
 		lblPassword.setBounds(86, 185, 142, 20);
@@ -194,26 +194,26 @@ public class CusLogin extends JFrame {
 		lblNewLabel_1_2_1_1.setBounds(280, 93, 38, 21);
 		
 		contentPane.add(lblNewLabel_1_2_1_1);
-		lblAddress.setEditable(false);
-		lblAddress.setColumns(10);
-		lblAddress.setBounds(378, 62, 142, 20);
+		Address.setEditable(false);
+		Address.setColumns(10);
+		Address.setBounds(378, 62, 142, 20);
 		
-		contentPane.add(lblAddress);
-		lblCity.setEditable(false);
-		lblCity.setColumns(10);
-		lblCity.setBounds(313, 94, 142, 20);
+		contentPane.add(Address);
+		City.setEditable(false);
+		City.setColumns(10);
+		City.setBounds(313, 94, 142, 20);
 		
-		contentPane.add(lblCity);
-		lblC_Country.setEditable(false);
-		lblC_Country.setColumns(10);
-		lblC_Country.setBounds(337, 121, 142, 20);
+		contentPane.add(City);
+		C_Country.setEditable(false);
+		C_Country.setColumns(10);
+		C_Country.setBounds(337, 121, 142, 20);
 		
-		contentPane.add(lblC_Country);
-		lblState.setEditable(false);
-		lblState.setColumns(10);
-		lblState.setBounds(323, 153, 142, 20);
+		contentPane.add(C_Country);
+		State.setEditable(false);
+		State.setColumns(10);
+		State.setBounds(323, 153, 142, 20);
 		
-		contentPane.add(lblState);
+		contentPane.add(State);
 		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_2_1.setBounds(352, 36, 154, 14);
 		
@@ -222,26 +222,26 @@ public class CusLogin extends JFrame {
 		p.setBounds(279, 184, 89, 21);
 		
 		contentPane.add(p);
-		lblPostalCode.setEditable(false);
-		lblPostalCode.setColumns(10);
-		lblPostalCode.setBounds(378, 185, 142, 20);
+		PostalCode.setEditable(false);
+		PostalCode.setColumns(10);
+		PostalCode.setBounds(378, 185, 142, 20);
 		
-		contentPane.add(lblPostalCode);
+		contentPane.add(PostalCode);
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblNewLabel_3.setBounds(10, 93, 89, 21);
 		
 		contentPane.add(lblNewLabel_3);
-		lblMiddleInitial.setEditable(false);
-		lblMiddleInitial.setColumns(10);
-		lblMiddleInitial.setBounds(96, 93, 25, 20);
+		MiddleInitial.setEditable(false);
+		MiddleInitial.setColumns(10);
+		MiddleInitial.setBounds(96, 93, 25, 20);
 		
-		contentPane.add(lblMiddleInitial);
+		contentPane.add(MiddleInitial);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblNewLabel.setBounds(11, 216, 62, 29);
 		
 		contentPane.add(lblNewLabel);
 		
-		contentPane.add(lblUserID);
+		contentPane.add(UserID);
 		
 		
 		 System.out.println("Connect to DB");
@@ -262,17 +262,17 @@ public class CusLogin extends JFrame {
 	           
 	           while(rs.next())
 	           {
-	        	   lblEmail.setText(rs.getString("Email"));
+	        	   Email.setText(rs.getString("Email"));
 	        	   lblFName.setText(rs.getString("First_name"));
 	        	   lblLName.setText(rs.getString("Last_Name"));
-	        	   lblMiddleInitial.setText(rs.getString("Middle_init"));
-	        	   lblUserID.setText(rs.getString("Account_ID"));
-	        	   lblAddress.setText(rs.getString("Default_Shipping_adress"));
-	        	   lblCity.setText(rs.getString("Default_Shipping_city"));
-	        	   lblPostalCode.setText(rs.getString("Default_SHipping_zip"));
-	        	   lblC_Country.setText(rs.getString("Default_shipping_country"));
-	        	   lblState.setText(rs.getString("Default_shipping_state"));
-	        	   
+	        	   MiddleInitial.setText(rs.getString("Middle_init"));
+	        	   UserID.setText(rs.getString("Account_ID"));
+	        	   Address.setText(rs.getString("Default_Shipping_adress"));
+	        	   City.setText(rs.getString("Default_Shipping_city"));
+	        	   PostalCode.setText(rs.getString("Default_SHipping_zip"));
+	        	   C_Country.setText(rs.getString("Default_shipping_country"));
+	        	   State.setText(rs.getString("Default_shipping_state"));
+	        	  
 	        	   
 	           }
 	         //step5 close the connection object
@@ -298,22 +298,22 @@ public class CusLogin extends JFrame {
 		lblNewLabel_1_2_1.setBounds(11, 125, 68, 21);
 		
 		contentPane.add(lblNewLabel_1_2_1);
-		FName.setColumns(10);
-		FName.setBounds(86, 62, 142, 20);
+		lblFName.setColumns(10);
+		lblFName.setBounds(86, 62, 142, 20);
 		
-		contentPane.add(FName);
-		LName.setColumns(10);
-		LName.setBounds(86, 126, 142, 20);
+		contentPane.add(lblFName);
+		lblLName.setColumns(10);
+		lblLName.setBounds(86, 126, 142, 20);
 		
-		contentPane.add(LName);
+		contentPane.add(lblLName);
 		Email.setColumns(10);
 		Email.setBounds(52, 153, 142, 20);
 		
 		contentPane.add(Email);
-		Password.setColumns(10);
-		Password.setBounds(86, 185, 142, 20);
+		lblPassword.setColumns(10);
+		lblPassword.setBounds(86, 185, 142, 20);
 		
-		contentPane.add(Password);
+		contentPane.add(lblPassword);
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_2.setBounds(62, 36, 122, 14);
 		
